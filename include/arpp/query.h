bc0ea38112da9fbbe0f9f4da32d5ca58032843e7
Hiroki Yagita <hiroki.yagita@gree.co.jp>
2015-04-16 22:58:46 +0900

    impls where query method and loading method

--- include/arpp/query.h
+++ include/arpp/query.h
@@ -1,14 +1,72 @@
 #pragma once
 #include <format.h>
+#include <map>
 #include <memory>
+#include <string>
 #include "connection.h"
 
 namespace arpp {
 
+enum class Order {
+  kAsc,
+  kDesc,
+};
+
+template <typename Model>
+class Project {
+ public:
+  using ModelPtr = std::shared_ptr<Model>;
+
+  enum class Type { kWhere, kOrder, kLimit };
+
+  Project(Type type, const std::string &exp) {
+    _conditions.insert(std::make_pair(type, exp));
+  }
+
+  std::vector<ModelPtr> load() {
+    if (!Connection::has_connected()) {
+      return std::move(std::vector<ModelPtr>());
+    }
+
+    auto for_schema = std::make_shared<Model>();
+    for_schema->connect(Connection::shared_connection());
+
+    fmt::MemoryWriter buf;
+    buf << "SELECT * FROM " << for_schema->table_name();
+    for (auto &one : _conditions) {
+      if (one.first == Type::kWhere) {
+        buf << " WHERE " << one.second;
+      } else if (one.first == Type::kOrder) {
+        buf << " ORDER BY " << one.second;
+      } else if (one.first == Type::kLimit) {
+        buf << " LIMIT " << one.second;
+      }
+    }
+
+    std::vector<ModelPtr> models;
+    auto c = Connection::shared_connection();
+    c->execute_sql_for_each(buf.str(), [&](const Connection::RowType &row) {
+      auto m = std::make_shared<Model>();
+      m->connect(Connection::shared_connection());
+      for (auto one : row) {
+        m->set_field(one);
+      }
+      models.emplace_back(m);
+    });
+
+    return std::move(models);
+  }
+
+ private:
+  std::map<Type, std::string> _conditions;
+};
+
 template <typename Model>
 class Query {
  public:
-  typedef std::shared_ptr<Model> ModelPtr;
+  using ModelPtr = std::shared_ptr<Model>;
+  using ProjectType = Project<Model>;
+  using ProjectPtr = std::shared_ptr<ProjectType>;
 
   static ModelPtr first() {
     return _exec_single([&](ModelPtr m, fmt::MemoryWriter &mw) {
@@ -27,6 +85,13 @@ class Query {
       mw << "SELECT * FROM " << m->table_name() << " WHERE id = " << id
          << " LIMIT 1;";
     });
+  }
+
+  template <typename... Args>
+  static ProjectPtr where(const std::string &cond, const Args &... args) {
+    auto q = fmt::format(cond, args...);
+
+    return std::make_shared<ProjectType>(ProjectType::Type::kWhere, q);
   }
 
  private:
