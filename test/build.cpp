3339f09c975ce1ec6296b129b192897d7de569e1
Hiroki Yagita <hiroki.yagita@gree.co.jp>
2015-05-09 01:35:36 +0900

    add test

--- test/build.cpp
+++ test/build.cpp
@@ -2,13 +2,15 @@
 #include <catch.hpp>
 #include <arpp/arpp.h>
 
-TEST_CASE("build is ok", "build") {
-	std::map<std::string, std::string> options = {
-	  {arpp::Connection::kOptionAdapter, "sqlite3"},
-	  {arpp::Connection::kOptionDatabase, "db.sqlite3"},
-	};
+SCENARIO("build is ok", "[build]") {
+  SECTION("build is ok") {
+    std::map<std::string, std::string> options = {
+        {arpp::Connection::kOptionAdapter, "sqlite3"},
+        {arpp::Connection::kOptionDatabase, "db.sqlite3"},
+    };
 
-	arpp::Status status;
-	auto c = arpp::Connection::connect(options, &status);
-	REQUIRE(status.is_ok());
+    arpp::Status status;
+    auto c = arpp::Connection::connect(options, &status);
+    REQUIRE(status.is_ok());
+  }
 }
